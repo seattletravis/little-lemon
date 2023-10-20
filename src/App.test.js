64cb4b69a92d3cd118798c93bb9e33148af8ca3d
Travis Lamberte <travis@lamberte.com>
2023-10-20 08:47:47 -0700

    add test to apptestjs file

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,19 @@
-import { render, screen } from '@testing-library/react';
+import { fireEvent, render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import { BrowserRouter } from 'react-router-dom';
 import App from './App';
+import BookingForm from './components/BookingForm';
+import Header from './components/Header';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test('renders the Header heading text', () => {
+  render(<BrowserRouter><App /></BrowserRouter>)
+  const headingElement = screen.getByAltText("Reserve Table")
+  expect(headingElement).toBeInTheDocument()
+
+
+const reserveButton = screen.getByRole("button")
+fireEvent.click(reserveButton)
+
+const headingElementNew = screen.getByText("Choose Date")
+expect(headingElementNew).toBeInTheDocument()
 });
