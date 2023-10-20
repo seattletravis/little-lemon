93ec1d93cfae70a69390c92dc517f198de508b45
Travis Lamberte <travis@lamberte.com>
2023-10-20 06:54:18 -0700

    add availabletimes array and map to dropdown on form

--- src/components/BookingForm.js
+++ src/components/BookingForm.js
@@ -1,30 +1,57 @@
+import { useEffect, useState } from 'react';
+
 //styles
 import "../App.css"
 
 
 function BookingForm() {
+
+  let availableTimes = [
+    { time: '17:00', available: true },
+    { time: '18:00', available: true },
+    { time: '19:00', available: true },
+    { time: '20:00', available: true },
+    { time: '21:00', available: true },
+    { time: '22:00', available: true },
+  ]
+
+  const [date, setDate] = useState({})
+  const [time, setTime] = useState({})
+  const [guests, setGuests] = useState('')
+  const [occasion, setOccasion] = useState('')
+
+  const handleSubmit = (e) => {
+    e.preventDefault()
+    console.log(date)
+  }
+
+  
+
   return (
     <form>
       <label htmlFor="res-date">Choose Date</label>
-      <input type="date" id="res-date" />
+      <input
+        onChange={e => setDate(e.target.value)}
+        type="date"
+        id="res-date"
+        required
+        value={date}
+      />
       <label htmlFor="res-time">Choose Time</label>
-      <select id="res-time">
-        <option>17:00</option>
-        <option>18:00</option>
-        <option>19:00</option>
-        <option>20:00</option>
-        <option>21:00</option>
-        <option>22:00</option>
+      <select onChange={e => setTime(e.target.value)} id="res-time" required>
+        {availableTimes.map(time => (
+          <option>{time.time}</option>
+        ))}
       </select>
-      <label htmlFor="guests">Number of guests</label>
-      <input type="number" placeholder='1'  />
-      <label htmlFor="">Occasion</label>
-      <select name="" id="">
+      <label htmlFor="guests" required>Number of guests</label>
+      <input onChange={e => setGuests(e.target.value)} type="number" placeholder='1' min="1" max="10" id="guests" />
+      <label htmlFor="occasion">Occasion</label>
+      <select onChange={e => setOccasion(e.target.value)} id="occasion">
         <option>Birthday</option>
         <option>Anniversary</option>
       </select>
-      <input type="submit" value="Make your reservation" />
-
+      <input onSubmit={handleSubmit} type="submit" value="Make your reservation" />
+      <button onClick={console.log(date)}>ConsoleLogIt</button>
     </form>
   )
 }
